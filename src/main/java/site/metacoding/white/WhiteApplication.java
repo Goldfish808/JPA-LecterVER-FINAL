28c59d5831ac68c6a0cc72d3014ec50450ccac0a
ssar <ssar@nate.com>
2022-10-21 10:56:19 +0900

    스프링부트 JPA 세팅 완료

---
+++ src/main/java/site/metacoding/white/WhiteApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.white;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteApplication.class, args);
+	}
+
+}
