7ba9ffdb411f6faa2083d006341236cbbc0bf9d1
ssar <ssar@nate.com>
2022-10-21 12:41:03 +0900

    DB 쿼리 콘솔 세팅 완료

--- src/main/java/site/metacoding/white/WhiteApplication.java
+++ src/main/java/site/metacoding/white/WhiteApplication.java
@@ -8,6 +8,7 @@ public class WhiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhiteApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/site/metacoding/white/domain/Board.java
@@ -0,0 +1,22 @@
+package site.metacoding.white.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(length = 1000)
+    private String content;
+}
