b01edefea592411a449828f93df2d844fc904306
ssar <ssar@nate.com>
2022-10-27 14:36:25 +0900

    SHA256 적용 - 회원가입, 로그인

---
+++ src/main/java/site/metacoding/white/util/SHA256.java
@@ -0,0 +1,30 @@
+package site.metacoding.white.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.stereotype.Component;
+
+// https://bamdule.tistory.com/233
+@Component
+public class SHA256 {
+
+    public String encrypt(String text) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(text.getBytes());
+            return bytesToHex(md.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("비밀번호 해싱이 실패하였습니다.");
+        }
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+        for (byte b : bytes) {
+            builder.append(String.format("%02x", b));
+        }
+        return builder.toString();
+    }
+
+}
