2cb886a28cd27d08a8ac0bedc9f05d01f5232b5b
ssar <ssar@nate.com>
2022-10-27 19:28:33 +0900

    통합 테스트 버그 해결 - MODE=MYSQL, @Sql 참조무결성 비활성화, 트랜잭션 롤백 안되는 것

--- src/test/java/site/metacoding/white/web/UserApiControllerTest.java
+++ src/test/java/site/metacoding/white/web/UserApiControllerTest.java
@@ -1,5 +1,98 @@
 package site.metacoding.white.web;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+import site.metacoding.white.dto.UserReqDto.JoinReqDto;
+
+@ActiveProfiles("test")
+// @Transactional // 통합테스트에서 RANDOM_PORT를 사용하면 새로운 스레드로 돌기 때문에 rollback 무의미
+@Sql("classpath:truncate.sql")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class UserApiControllerTest {
+
+    @Autowired
+    private TestRestTemplate rt;
+    @Autowired
+    private ObjectMapper om;
+
+    private static HttpHeaders headers;
+
+    @BeforeAll
+    public static void init() {
+        headers = new HttpHeaders(); // http 요청 header에 필요
+        headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Order(1)
+    @Test
+    public void join_test() throws JsonProcessingException {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("very");
+        joinReqDto.setPassword("1234");
+
+        String body = om.writeValueAsString(joinReqDto);
+        System.out.println(body);
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+                request, String.class);
+
+        // then
+        // System.out.println(response.getStatusCode());
+        // System.out.println(response.getBody());
+
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        // System.out.println(dc.jsonString());
+        Integer code = dc.read("$.code");
+        Assertions.assertThat(code).isEqualTo(1);
+    }
+
+    @Order(2)
+    @Test
+    public void join_test2() throws JsonProcessingException {
+        // given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setUsername("very");
+        joinReqDto.setPassword("1234");
+
+        String body = om.writeValueAsString(joinReqDto);
+        System.out.println(body);
+
+        // when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/join", HttpMethod.POST,
+                request, String.class);
+
+        // then
+        // System.out.println(response.getStatusCode());
+        // System.out.println(response.getBody());
+
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        // System.out.println(dc.jsonString());
+        Integer code = dc.read("$.code");
+        Assertions.assertThat(code).isEqualTo(1);
+    }
 
 }
